f69cd496a7a4b482365d025c816d456575a49fb2
Md salim islam <salimislam0036@gmail.com>
2026-03-24 08:55:37 +0600

    fix: remove p-limit, use native Promise.all for image uploads

--- routes/category.js
+++ routes/category.js
@@ -1,9 +1,6 @@
-// const category = require("../models/category");
 const Category = require("../models/category");
 const express = require("express");
 const router = express.Router();
-// const pLimit = require("p-limit").default;
-const pLimit = require("p-limit");
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
@@ -12,23 +9,21 @@ cloudinary.config({
   api_secret: process.env.CLOUD_API_SECRET_KEY,
 });
 
-// get all category
+// ==========================
+// GET ALL CATEGORIES
+// ==========================
 router.get("/", async (req, res) => {
   try {
-    const page = parseInt(req.query.page) || 1;
-   // ১. এখানে পরিবর্তন: ফ্রন্টএন্ড থেকে আসা limit রিড করুন, না থাকলে ডিফল্ট ৬ দিন
+    const page    = parseInt(req.query.page)  || 1;
     const perPage = parseInt(req.query.limit) || 6;
-    
-    // মোট ক্যাটাগরি সংখ্যা বের করা
+
     const totalPosts = await Category.countDocuments();
     const totalPages = Math.ceil(totalPosts / perPage);
 
-    // যদি পেজ সংখ্যা টোটাল পেজের চেয়ে বেশি হয় (এবং ডেটা থাকে)
     if (totalPosts > 0 && page > totalPages) {
       return res.status(404).json({ message: "Page not found" });
     }
 
-    // ডেটা ফেচ করা
     const categoryList = await Category.find()
       .skip((page - 1) * perPage)
       .limit(perPage)
@@ -39,161 +34,324 @@ router.get("/", async (req, res) => {
     }
 
     return res.status(200).json({
-      categoryList: categoryList,
-      totalPages: totalPages,
+      categoryList,
+      totalPages,
       currentPage: page,
-      totalItems: totalPosts
+      totalItems: totalPosts,
     });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 });
+
+// ==========================
+// GET SINGLE CATEGORY BY ID
+// ==========================
+router.get("/:id", async (req, res) => {
+  try {
+    const category = await Category.findById(req.params.id);
+    if (!category) {
+      return res.status(404).json({ success: false, message: "Category not found" });
+    }
+    res.status(200).json({ success: true, data: category });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+});
+
+// ==========================
+// DELETE CATEGORY BY ID
+// ==========================
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedCategory = await Category.findByIdAndDelete(req.params.id);
+    if (!deletedCategory) {
+      return res.status(404).json({ success: false, message: "Category not found" });
+    }
+    res.status(200).json({ success: true, message: "Category deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+});
+
+// ==========================
+// CREATE CATEGORY
+// ==========================
+router.post("/create", async (req, res) => {
+  try {
+    if (!req.body.images || !Array.isArray(req.body.images)) {
+      return res.status(400).json({ error: "Images array is required" });
+    }
+
+    const imagesToUpload = req.body.images.map(async (image) => {
+      const result = await cloudinary.uploader.upload(image);
+      return result;
+    });
+
+    const uploadStatus = await Promise.all(imagesToUpload);
+    const imgurl = uploadStatus.map((item) => item.secure_url);
+
+    let category = new Category({
+      name:   req.body.name,
+      images: imgurl,
+      color:  req.body.color,
+    });
+
+    category = await category.save();
+    res.status(201).json(category);
+  } catch (err) {
+    res.status(500).json({ error: err.message, success: false });
+  }
+});
+
+// ==========================
+// UPDATE CATEGORY BY ID
+// ==========================
+router.put("/:id", async (req, res) => {
+  try {
+    if (!req.body.images || !Array.isArray(req.body.images)) {
+      return res.status(400).json({ error: "Images array is required" });
+    }
+
+    const imagesToUpload = req.body.images.map(async (image) => {
+      const result = await cloudinary.uploader.upload(image);
+      return result;
+    });
+
+    const uploadStatus = await Promise.all(imagesToUpload);
+    const imgurl = uploadStatus.map((item) => item.secure_url);
+
+    const category = await Category.findByIdAndUpdate(
+      req.params.id,
+      { name: req.body.name, images: imgurl, color: req.body.color },
+      { returnDocument: "after" }
+    );
+
+    if (!category) {
+      return res.status(500).json({ message: "Category cannot be updated", success: false });
+    }
+
+    res.send(category);
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+});
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // const category = require("../models/category");
+// const Category = require("../models/category");
+// const express = require("express");
+// const router = express.Router();
+// // const pLimit = require("p-limit").default;
+// const pLimit = require("p-limit");
+// const cloudinary = require("cloudinary").v2;
+
+// cloudinary.config({
+//   cloud_name: process.env.CLOUD_NAME,
+//   api_key: process.env.CLOUD_API_KEY,
+//   api_secret: process.env.CLOUD_API_SECRET_KEY,
+// });
+
+// // get all category
 // router.get("/", async (req, res) => {
 //   try {
-//     const categoryList = await Category.find(); 
+//     const page = parseInt(req.query.page) || 1;
+//    // ১. এখানে পরিবর্তন: ফ্রন্টএন্ড থেকে আসা limit রিড করুন, না থাকলে ডিফল্ট ৬ দিন
+//     const perPage = parseInt(req.query.limit) || 6;
+    
+//     // মোট ক্যাটাগরি সংখ্যা বের করা
+//     const totalPosts = await Category.countDocuments();
+//     const totalPages = Math.ceil(totalPosts / perPage);
+
+//     // যদি পেজ সংখ্যা টোটাল পেজের চেয়ে বেশি হয় (এবং ডেটা থাকে)
+//     if (totalPosts > 0 && page > totalPages) {
+//       return res.status(404).json({ message: "Page not found" });
+//     }
+
+//     // ডেটা ফেচ করা
+//     const categoryList = await Category.find()
+//       .skip((page - 1) * perPage)
+//       .limit(perPage)
+//       .exec();
+
 //     if (!categoryList) {
 //       return res.status(500).json({ success: false });
 //     }
-//     res.json(categoryList);
+
+//     return res.status(200).json({
+//       categoryList: categoryList,
+//       totalPages: totalPages,
+//       currentPage: page,
+//       totalItems: totalPosts
+//     });
 //   } catch (err) {
 //     res.status(500).json({ error: err.message });
 //   }
 // });
+// // router.get("/", async (req, res) => {
+// //   try {
+// //     const categoryList = await Category.find(); 
+// //     if (!categoryList) {
+// //       return res.status(500).json({ success: false });
+// //     }
+// //     res.json(categoryList);
+// //   } catch (err) {
+// //     res.status(500).json({ error: err.message });
+// //   }
+// // });
 
-// GET single category by ID
-router.get("/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
+// // GET single category by ID
+// router.get("/:id", async (req, res) => {
+//   try {
+//     const { id } = req.params;
 
-    // Check valid ID
-    const category = await Category.findById(id);
-    if (!category) {
-      return res.status(404).json({
-        success: false,
-        message: "Category not found",
-      });
-    }
+//     // Check valid ID
+//     const category = await Category.findById(id);
+//     if (!category) {
+//       return res.status(404).json({
+//         success: false,
+//         message: "Category not found",
+//       });
+//     }
 
-    res.status(200).json({
-      success: true,
-      data: category,
-    });
-  } catch (error) {
-    console.error("Get Category Error:", error);
-    res.status(500).json({
-      success: false,
-      message: error.message,
-    });
-  }
-});
+//     res.status(200).json({
+//       success: true,
+//       data: category,
+//     });
+//   } catch (error) {
+//     console.error("Get Category Error:", error);
+//     res.status(500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// });
 
-// DELETE Category by ID
-router.delete("/:id", async (req, res) => {
-  try {
-    const deletedCategory = await Category.findByIdAndDelete(req.params.id);
+// // DELETE Category by ID
+// router.delete("/:id", async (req, res) => {
+//   try {
+//     const deletedCategory = await Category.findByIdAndDelete(req.params.id);
 
-    if (!deletedCategory) {
-      return res.status(404).json({
-        success: false,
-        message: "Category not found",
-      });
-    }
+//     if (!deletedCategory) {
+//       return res.status(404).json({
+//         success: false,
+//         message: "Category not found",
+//       });
+//     }
 
-    res.status(200).json({
-      success: true,
-      message: "Category deleted successfully",
-    });
-  } catch (error) {
-    console.error("Delete Category Error:", error);
-    res.status(500).json({
-      success: false,
-      message: error.message,
-    });
-  }
-});
+//     res.status(200).json({
+//       success: true,
+//       message: "Category deleted successfully",
+//     });
+//   } catch (error) {
+//     console.error("Delete Category Error:", error);
+//     res.status(500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// });
 
-// CREATE category
-router.post("/create", async (req, res) => {
-  try {
-    if (!req.body.images || !Array.isArray(req.body.images)) {
-      return res.status(400).json({
-        error: "Images array is required",
-      });
-    }
+// // CREATE category
+// router.post("/create", async (req, res) => {
+//   try {
+//     if (!req.body.images || !Array.isArray(req.body.images)) {
+//       return res.status(400).json({
+//         error: "Images array is required",
+//       });
+//     }
 
-    const limit = pLimit(2);
+//     const limit = pLimit(2);
 
-    const imagesToUpload = req.body.images.map((image) =>
-      limit(async () => {
-        const result = await cloudinary.uploader.upload(image);
-        return result;
-      }),
-    );
+//     const imagesToUpload = req.body.images.map((image) =>
+//       limit(async () => {
+//         const result = await cloudinary.uploader.upload(image);
+//         return result;
+//       }),
+//     );
 
-    const uploadStatus = await Promise.all(imagesToUpload);
+//     const uploadStatus = await Promise.all(imagesToUpload);
 
-    const imgurl = uploadStatus.map((item) => item.secure_url);
+//     const imgurl = uploadStatus.map((item) => item.secure_url);
 
-    let category = new Category({
-      name: req.body.name,
-      images: imgurl,
-      color: req.body.color,
-    });
+//     let category = new Category({
+//       name: req.body.name,
+//       images: imgurl,
+//       color: req.body.color,
+//     });
 
-    category = await category.save();
+//     category = await category.save();
 
-    res.status(201).json(category);
-  } catch (err) {
-    res.status(500).json({
-      error: err.message,
-      success: false,
-    });
-  }
-});
+//     res.status(201).json(category);
+//   } catch (err) {
+//     res.status(500).json({
+//       error: err.message,
+//       success: false,
+//     });
+//   }
+// });
 
-// UPDATE Category by ID
-router.put("/:id", async (req, res) => {
-  const limit = pLimit(2);
+// // UPDATE Category by ID
+// router.put("/:id", async (req, res) => {
+//   const limit = pLimit(2);
 
-  if (!req.body.images || !Array.isArray(req.body.images)) {
-      return res.status(400).json({
-        error: "Images array is required",
-      });
-    }
+//   if (!req.body.images || !Array.isArray(req.body.images)) {
+//       return res.status(400).json({
+//         error: "Images array is required",
+//       });
+//     }
 
-  const imagesToUpload = req.body.images.map((image) =>
-    limit(async () => {
-      const result = await cloudinary.uploader.upload(image);
-      return result;
-    }),
-  );
+//   const imagesToUpload = req.body.images.map((image) =>
+//     limit(async () => {
+//       const result = await cloudinary.uploader.upload(image);
+//       return result;
+//     }),
+//   );
 
-  const uploadStatus = await Promise.all(imagesToUpload);
-  const imgurl = uploadStatus.map((item) => item.secure_url);
-  if (!uploadStatus) {
-    return res.status(500).json({
-      error: "images cannot be uploaded",
-      status: false,
-    });
-  }
-  const category = await Category.findByIdAndUpdate(
-    req.params.id,
-    {
-      name: req.body.name,
-      images: imgurl,
-      color: req.body.color,
-    },
-     { returnDocument: "after" },
-  );
+//   const uploadStatus = await Promise.all(imagesToUpload);
+//   const imgurl = uploadStatus.map((item) => item.secure_url);
+//   if (!uploadStatus) {
+//     return res.status(500).json({
+//       error: "images cannot be uploaded",
+//       status: false,
+//     });
+//   }
+//   const category = await Category.findByIdAndUpdate(
+//     req.params.id,
+//     {
+//       name: req.body.name,
+//       images: imgurl,
+//       color: req.body.color,
+//     },
+//      { returnDocument: "after" },
+//   );
 
-  if (!category) {
-    return res.status(500).json({
-      message: "Category cannot be updated",
-      success: false,
-    });
-  }
+//   if (!category) {
+//     return res.status(500).json({
+//       message: "Category cannot be updated",
+//       success: false,
+//     });
+//   }
 
-  res.send(category);
-});
+//   res.send(category);
+// });
 
-module.exports = router;
+// module.exports = router;
